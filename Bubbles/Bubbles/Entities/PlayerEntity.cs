13de58d044553f6b4238c1bfaaf42f528d2e5ea7
Peter Mandile <peterm1994@hotmail.com>
2018-11-10 16:08:03 +1100

    Added animation to player

--- Bubbles/Bubbles/Entities/PlayerEntity.cs
+++ Bubbles/Bubbles/Entities/PlayerEntity.cs
@@ -1,18 +1,32 @@
-using System.IO;
 using Bubbles.Components;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Nez;
 using Nez.Sprites;
+using Nez.Textures;
 
 namespace Bubbles.Entities
 {
     public class PlayerEntity : Entity
     {
+        enum Animations
+        {
+            Walk
+        }
+
         public PlayerEntity() : base("Player")
         {
-            var tex = Texture2D.FromStream(Core.graphicsDevice, File.OpenRead("../../Content/textures/player.png"));
-            addComponent(new Sprite(tex));
+            // Load the textures in
+            var tex = Core.content.Load<Texture2D>("player");
+            var subTextures = Subtexture.subtexturesFromAtlas(tex, 32, tex.Height);
+
+            // Create the sprite component with the first frame loaded by default.
+            var sprite = addComponent(new Sprite<Animations>(subTextures[0]));
+
+            // Register the walk animation and start it
+            sprite.addAnimation(Animations.Walk, new SpriteAnimation(subTextures));
+            sprite.play(Animations.Walk);
+
             addComponent(new Player());
             addComponent(new PlayerControlled());
             addComponent(new Motion());
